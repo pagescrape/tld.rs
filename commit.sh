ec201d52eb8092f2363cc57dafe7a8a3d5c34814
Ernestas Poskus <ernestas.poskus@gmail.com>
2017-10-30 19:01:31 +0200

    Version 2017102901, Last Updated Mon Oct 30 07:07:02 2017 UTC

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 git add .
 message=`curl https://data.iana.org/TLD/tlds-alpha-by-domain.txt | head -n 1 | sed 's/# //'`
-git commit -m "$message"
+git commit -m "$message" && git push && cargo publish
